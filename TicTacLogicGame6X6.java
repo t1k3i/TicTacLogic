c8b597c2101d005f15f20fd71a4eb7620238f31b
t1k3i <radovac.tikei@gmail.com>
2024-03-14 14:02:53 +0100

    Implemented solve method

--- TicTacLogicGame6X6.java
+++ TicTacLogicGame6X6.java
@@ -13,8 +13,312 @@ public class TicTacLogicGame6X6 {
         this.generateGrid();
     }
 
-    public void solve() {
-        // TODO
+    public boolean solve() {
+        while (!this.full()) {
+            int[] pair = this.findPair();
+            if (pair != null) {
+                this.grid[pair[0]][pair[1]] = pair[2] == 1 ? 'X' : 'O';
+                continue;
+            }
+            int[] fullLine = this.findFullLine();
+            if (fullLine != null) {
+                this.grid[fullLine[0]][fullLine[1]] = fullLine[2] == 1 ? 'X' : 'O';
+                continue;
+            }
+            int[] differentLine = this.findDifferentLine();
+            if (differentLine == null) return false;
+            this.grid[differentLine[0]][differentLine[1]] = differentLine[2] == 1 ? 'X' : 'O';
+            this.grid[differentLine[3]][differentLine[4]] = differentLine[5] == 1 ? 'X' : 'O';
+        }
+        return true;
+    }
+    
+    private int[] findDifferentLine() {
+        int[] pair = new int[6];
+        // Check rows
+        Set<String> rowSet = new HashSet<>();
+        for (int i = 0; i < this.grid.length; i++) {
+            int xR = 0, oR = 0;
+            StringBuilder row = new StringBuilder("");
+            for (int j = 0; j < this.grid[i].length; j++) {
+                if (this.grid[i][j] == 'X') {
+                    row.append('X');
+                    xR++;
+                } else if (this.grid[i][j] == 'O') {
+                    row.append('O');
+                    oR++;
+                } else {
+                    row.append('.');
+                }
+            }
+            boolean contains = false;
+            for (int k = 0; k < row.length(); k++) {
+                if (row.charAt(k) == '.') {
+                    contains = true;
+                    break;
+                }
+            }
+            if (!contains) {
+                rowSet.add(row.toString());
+            }
+            if (xR + oR == 4 && rowSet.size() >= 1) {
+                int j1 = -1, j2 = -1;
+                for (int j = 0; j < this.grid[i].length; j++) {
+                    if (this.grid[i][j] == 0 && j1 == -1) {
+                        j1 = j;
+                    } else if (this.grid[i][j] == 0 && j2 == -1) {
+                        j2 = j;
+                    }
+                }
+                row.replace(j1, j1, "X");
+                row.replace(j2, j2, "O");
+                if (rowSet.contains(row.toString())) {
+                    row.replace(j1, j1, "O");
+                    row.replace(j2, j2, "X");
+                    if (rowSet.contains(row.toString())) return null;
+                }
+                pair[0] = i;
+                pair[1] = j1;
+                pair[2] = row.charAt(j1) == 'X' ? 1 : 0;
+                pair[3] = i;
+                pair[4] = j2;
+                pair[5] = row.charAt(j2) == 'X' ? 1 : 0;
+                return pair;
+            }
+        }
+        // Check collums
+        Set<String> collumSet = new HashSet<>();
+        for (int j = 0; j < this.grid[0].length; j++) {
+            int xC = 0, oC = 0;
+            StringBuilder collum = new StringBuilder("");
+            for (int i = 0; i < this.grid.length; i++) {
+                if (this.grid[i][j] == 'X') {
+                    collum.append('X');
+                    xC++;
+                } else if (this.grid[i][j] == 'O') {
+                    collum.append('O');
+                    oC++;
+                } else {
+                    collum.append('.');
+                }
+            }
+            boolean contains = false;
+            for (int k = 0; k < collum.length(); k++) {
+                if (collum.charAt(k) == '.') {
+                    contains = true;
+                    break;
+                }
+            }
+            if (!contains) {
+                collumSet.add(collum.toString());
+            }
+            if (xC + oC == 4 && collumSet.size() >= 1) {
+                int i1 = -1, i2 = -1;
+                for (int i = 0; i < this.grid.length; i++) {
+                    if (this.grid[i][j] == 0 && i1 == -1) {
+                        i1 = i;
+                    } else if (this.grid[i][j] == 0 && i2 == -1) {
+                        i2 = i;
+                    }
+                }
+                collum.replace(i1, i1, "X");
+                collum.replace(i2, i2, "O");
+                if (collumSet.contains(collum.toString())) {
+                    collum.replace(i1, i1, "O");
+                    collum.replace(i2, i2, "X");
+                    if (collumSet.contains(collum.toString())) return null;
+                }
+                pair[0] = i1;
+                pair[1] = j;
+                pair[2] = collum.charAt(i1) == 'X' ? 1 : 0;
+                pair[3] = i2;
+                pair[4] = j;
+                pair[5] = collum.charAt(i2) == 'X' ? 1 : 0;
+                return pair;
+            }
+        }
+        return null;
+    }
+
+    private int[] findFullLine() {
+        int[] pair = new int[3];
+        // Check rows
+        for (int i = 0; i < this.grid.length; i++) {
+            int xR = 0; int oR = 0;
+            for (int j = 0; j < this.grid[i].length; j++) {
+                if (this.grid[i][j] == 'X') {
+                    xR++;
+                } else if (this.grid[i][j] == 'O') {
+                   oR++;
+                }
+            }
+            if (xR == 3) {
+                for (int j = 0; j < this.grid[i].length; j++) {
+                    if (this.grid[i][j] == 0) {
+                        pair[0] = i;
+                        pair[1] = j;
+                        pair[2] = 0;
+                        return pair;
+                    }
+                }
+            } else if (oR == 3) {
+                for (int j = 0; j < this.grid[i].length; j++) {
+                    if (this.grid[i][j] == 0) {
+                        pair[0] = i;
+                        pair[1] = j;
+                        pair[2] = 1;
+                        return pair;
+                    }
+                }
+            }
+        }
+        // Check collums
+        for (int j = 0; j < this.grid[0].length; j++) {
+            int xC = 0; int oC = 0;
+            for (int i = 0; i < this.grid.length; i++) {
+                if (this.grid[i][j] == 'X') {
+                    xC++;
+                } else if (this.grid[i][j] == 'O') {
+                   oC++;
+                }
+            }
+            if (xC == 3) {
+                for (int i = 0; i < this.grid.length; i++) {
+                    if (this.grid[i][j] == 0) {
+                        pair[0] = i;
+                        pair[1] = j;
+                        pair[2] = 0;
+                        return pair;
+                    }
+                }
+            } else if (oC == 3) {
+                for (int i = 0; i < this.grid.length; i++) {
+                    if (this.grid[i][j] == 0) {
+                        pair[0] = i;
+                        pair[1] = j;
+                        pair[2] = 1;
+                        return pair;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    private int[] findPair() {
+        int[] pair = new int[3];
+        // Check rows
+        for (int i = 0; i < this.grid.length; i++) {
+            int xR2 = 0; int oR2 = 0;
+            for (int j = 0; j < this.grid[i].length; j++) {
+                if (this.grid[i][j] == 'X') {
+                    xR2++; oR2 = 0;
+                    if (xR2 == 2) {
+                        if (j + 1 < this.grid[i].length && this.grid[i][j + 1] == 0) {
+                            pair[0] = i;
+                            pair[1] = j + 1;
+                            pair[2] = 0;
+                            return pair;
+                        } else if (j - 2 >= 0 && this.grid[i][j - 2] == 0) {
+                            pair[0] = i;
+                            pair[1] = j - 2;
+                            pair[2] = 0;
+                            return pair;
+                        }
+                    }
+                }
+                else if (this.grid[i][j] == 'O') {
+                    oR2++; xR2 = 0;
+                    if (oR2 == 2) {
+                        if (j + 1 < this.grid[i].length && this.grid[i][j + 1] == 0) {
+                            pair[0] = i;
+                            pair[1] = j + 1;
+                            pair[2] = 1;
+                            return pair;
+                        } else if (j - 2 >= 0 && this.grid[i][j - 2] == 0) {
+                            pair[0] = i;
+                            pair[1] = j - 2;
+                            pair[2] = 1;
+                            return pair;
+                        }
+                    }
+                } else if (this.grid[i][j] == 0 && j > 0 && j < this.grid[i].length - 1) {
+                    xR2 = 0; oR2 = 0;
+                    if (this.grid[i][j - 1] == 'X' && this.grid[i][j + 1] == 'X') {
+                        pair[0] = i;
+                        pair[1] = j;
+                        pair[2] = 0;
+                        return pair;
+                    } else if (this.grid[i][j - 1] == 'O' && this.grid[i][j + 1] == 'O') {
+                        pair[0] = i;
+                        pair[1] = j;
+                        pair[2] = 1;
+                        return pair;
+                    }
+                } else {xR2 = 0; oR2 = 0;}
+            }
+        }
+        // Check collums
+        for (int j = 0; j < this.grid[0].length; j++) {
+            int xC2 = 0; int oC2 = 0;
+            for (int i = 0; i < this.grid.length; i++) {
+                if (this.grid[i][j] == 'X') {
+                    xC2++; oC2 = 0;
+                    if (xC2 == 2) {
+                        if (i + 1 < this.grid.length && this.grid[i + 1][j] == 0) {
+                            pair[0] = i + 1;
+                            pair[1] = j;
+                            pair[2] = 0;
+                            return pair;
+                        } else if (i - 2 >= 0 && this.grid[i - 2][j] == 0) {
+                            pair[0] = i - 2;
+                            pair[1] = j;
+                            pair[2] = 0;
+                            return pair;
+                        }
+                    }
+                }
+                else if (this.grid[i][j] == 'O') {
+                    oC2++; xC2 = 0;
+                    if (oC2 == 2) {
+                        if (i + 1 < this.grid.length && this.grid[i + 1][j] == 0) {
+                            pair[0] = i + 1;
+                            pair[1] = j;
+                            pair[2] = 1;
+                            return pair;
+                        } else if (i - 2 >= 0 && this.grid[i - 2][j] == 0) {
+                            pair[0] = i - 2;
+                            pair[1] = j;
+                            pair[2] = 1;
+                            return pair;
+                        }
+                    }
+                } else if (this.grid[i][j] == 0 && i > 0 && i < this.grid.length - 1) {
+                    xC2 = 0; oC2 = 0;
+                    if (this.grid[i - 1][j] == 'X' && this.grid[i + 1][j] == 'X') {
+                        pair[0] = i;
+                        pair[1] = j;
+                        pair[2] = 0;
+                        return pair;
+                    } else if (this.grid[i - 1][j] == 'O' && this.grid[i + 1][j] == 'O') {
+                        pair[0] = i;
+                        pair[1] = j;
+                        pair[2] = 1;
+                        return pair;
+                    }
+                } else {xC2 = 0; oC2 = 0;}
+            }
+        }
+        return null;
+    }
+
+    private boolean full() {
+        for (int i = 0; i < this.grid.length; i++) {
+            for (int j = 0; j < this.grid[i].length; j++) {
+                if (this.grid[i][j] == 0) return false;
+            }
+        }
+        return true;
     }
 
     public void print() {
@@ -34,6 +338,7 @@ public class TicTacLogicGame6X6 {
         // TODO
         this.grid = new char[6][6];
         this.generateFullGrid(6, 0, 0, 0);
+        this.makePuzzle();
     }
 
     private boolean generateFullGrid(int ranNumberOfSquares, int ix, int i, int j) {
@@ -73,10 +378,12 @@ public class TicTacLogicGame6X6 {
             }
         }
         return true;
-
     }
 
-    // DONE??
+    private void makePuzzle() {
+        // TODO
+    }
+
     public boolean check() {
         // Check rows
         Set<String> rowSet = new HashSet<>();
